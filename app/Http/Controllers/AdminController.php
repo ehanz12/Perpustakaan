e54b298b02c438e3a17eead4f04de2895ccfb721
ehanz12 <rhanss@gmail.com>
2025-05-22 21:13:42 +0700

    PEMBUKAAN REGISTER ADMIN DAN USERS

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id())
+        {
+           $usertype = Auth::user()->usertype;
+
+           if($usertype == 'admin')
+           {
+            return view('admin.index');
+           } 
+           else if($usertype == 'user')
+           {
+            return view('home.index');
+           }
+        }
+
+        else
+        {
+            return redirect()->back();
+        }
+    }
+}
